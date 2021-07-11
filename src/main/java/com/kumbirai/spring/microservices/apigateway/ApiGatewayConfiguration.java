bd2b47ef3914df0c4ae7e7c03b43d9eed23a916c
Kumbirai Mundangepfupfu <kumbirai.mundangepfupfu@mmltd.co.za>
2021-07-11 19:29:06 +0200

    IntelliJ Reformat

--- src/main/java/com/kumbirai/spring/microservices/apigateway/ApiGatewayConfiguration.java
+++ src/main/java/com/kumbirai/spring/microservices/apigateway/ApiGatewayConfiguration.java
@@ -24,51 +24,53 @@ import org.springframework.context.annotation.Configuration;
  *
  * <b>Revision:</b>
  * <br>
- *					
  */
 @Configuration
 public class ApiGatewayConfiguration
 {
-	private static final Logger LOGGER = LogManager.getLogger(ApiGatewayConfiguration.class.getName());
-	private static final String URI_HTTP_HTTPBIN_ORG_80 = "http://httpbin.org:80";
-	private static final String URI_LB_CURRENCY_EXCHANGE = "lb://currency-exchange";
-	private static final String URI_LB_CURRENCY_CONVERSION = "lb://currency-conversion";
+    private static final Logger LOGGER = LogManager.getLogger(ApiGatewayConfiguration.class.getName());
+    private static final String URI_HTTP_HTTPBIN_ORG_80 = "http://httpbin.org:80";
+    private static final String URI_LB_LIMITS_SERVICE = "lb://limits-service";
+    private static final String URI_LB_CURRENCY_EXCHANGE = "lb://currency-exchange";
+    private static final String URI_LB_CURRENCY_CONVERSION = "lb://currency-conversion";
 
-	/**
-	 * Constructor:
-	 */
-	public ApiGatewayConfiguration()
-	{
-		super();
-		LOGGER.trace("ApiGatewayConfiguration.CONSTRUCTOR");
-	}
+    /**
+     * Constructor:
+     */
+    public ApiGatewayConfiguration()
+    {
+        super();
+        LOGGER.trace("ApiGatewayConfiguration.CONSTRUCTOR");
+    }
 
-	/**
-	 * Purpose:<br>
-	 * <br>
-	 * gatewayRouter<br>
-	 * <br>
-	 * @param builder
-	 * @return<br>
-	 * <br>
-	 */
-	@Bean
-	public RouteLocator gatewayRouter(RouteLocatorBuilder builder)
-	{
-		return builder.routes()
-				.route(p -> p.path("/get")
-						.filters(f -> f.addRequestHeader("MyHeader", "MyURI")
-								.addRequestParameter("Param", "MyValue"))
-						.uri(URI_HTTP_HTTPBIN_ORG_80))
-				.route(p -> p.path("/currency-exchange/**")
-						.uri(URI_LB_CURRENCY_EXCHANGE))
-				.route(p -> p.path("/currency-conversion/**")
-						.uri(URI_LB_CURRENCY_CONVERSION))
-				.route(p -> p.path("/currency-conversion-feign/**")
-						.uri(URI_LB_CURRENCY_CONVERSION))
-				.route(p -> p.path("/currency-conversion-new/**")
-						.filters(f -> f.rewritePath("/currency-conversion-new/(?<segment>.*)", "/currency-conversion-feign/${segment}"))
-						.uri(URI_LB_CURRENCY_CONVERSION))
-				.build();
-	}
+    /**
+     * Purpose:<br>
+     * <br>
+     * gatewayRouter<br>
+     * <br>
+     *
+     * @param builder -
+     * @return <br> <br>
+     */
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder)
+    {
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f.addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri(URI_HTTP_HTTPBIN_ORG_80))
+                .route(p -> p.path("/limits-service/**")
+                        .uri(URI_LB_LIMITS_SERVICE))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri(URI_LB_CURRENCY_EXCHANGE))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri(URI_LB_CURRENCY_CONVERSION))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri(URI_LB_CURRENCY_CONVERSION))
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath("/currency-conversion-new/(?<segment>.*)", "/currency-conversion-feign/${segment}"))
+                        .uri(URI_LB_CURRENCY_CONVERSION))
+                .build();
+    }
 }
---
+++ src/main/java/com/kumbirai/spring/microservices/apigateway/LoggingFilter.java
@@ -0,0 +1,53 @@
+/*
+ Copyright (c) 2021
+
+ All rights reserved.
+ */
+package com.kumbirai.spring.microservices.apigateway;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * <p><b>Purpose:</b><br>
+ * <br>
+ *
+ * <p><b>Title:</b> LoggingFilter<br>
+ * <br>
+ *
+ * @author Kumbirai 'Coach' Mundangepfupfu<br>
+ * @version 1.0<br>
+ *
+ * <b>Revision:</b>
+ * <br>
+ */
+public class LoggingFilter implements GlobalFilter
+{
+    private static final Logger LOGGER = LogManager.getLogger(LoggingFilter.class.getName());
+
+    /**
+     * Constructor:
+     */
+    public LoggingFilter()
+    {
+        super();
+		LOGGER.trace("LoggingFilter.CONSTRUCTOR");
+    }
+
+    /**
+     * (non-Javadoc)
+     *
+     * @see org.springframework.cloud.gateway.filter.GlobalFilter#filter(org.springframework.web.server.ServerWebExchange, org.springframework.cloud.gateway.filter.GatewayFilterChain)
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
+    {
+        LOGGER.info("Path of the request received -> {}", exchange.getRequest()
+                .getPath());
+        return chain.filter(exchange);
+    }
+}
