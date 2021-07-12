0521c6cef0dcd98719f5967f2cff57dc1956fe8f
Kumbirai Mundangepfupfu <kumbirai.mundangepfupfu@mmltd.co.za>
2021-07-12 15:30:43 +0200

    Some Code Cleanup

--- src/main/java/com/kumbirai/spring/microservices/apigateway/LoggingFilter.java
+++ src/main/java/com/kumbirai/spring/microservices/apigateway/LoggingFilter.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -25,6 +26,7 @@ import reactor.core.publisher.Mono;
  * <b>Revision:</b>
  * <br>
  */
+@Component
 public class LoggingFilter implements GlobalFilter
 {
     private static final Logger LOGGER = LogManager.getLogger(LoggingFilter.class.getName());
@@ -35,7 +37,7 @@ public class LoggingFilter implements GlobalFilter
     public LoggingFilter()
     {
         super();
-		LOGGER.trace("LoggingFilter.CONSTRUCTOR");
+        LOGGER.trace("LoggingFilter.CONSTRUCTOR");
     }
 
     /**
